8a2dec949f03cf3d6b7485b4eb61e5f244e46a28
Haoran Zhou <kens0serious@gmail.com>
2022-02-15 23:39:19 -0500

    added more options for category selection

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -33,6 +33,7 @@ const SetupForm = () => {
             >
               <option value="sports">sports</option>
               <option value="history">history</option>
+              <option value="politics">politics</option>
             </select>
           </div>
 
